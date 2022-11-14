0f0b96956e11633afb00f3220a8d05461a59141e
zakzinzan123 <zakzinzan123@gmail.com>
2022-11-14 23:24:17 +0800

    Make numArray push numbers properly and change style slightly

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
+let numArr = [];
+let op, num = '';
 const display = document.querySelector('#output');
 const operators = document.querySelectorAll('#operators > button');
 const numbers = document.querySelectorAll('.number');
 const clear = document.querySelector('#clear');
 const equals = document.querySelector('#equals');
-
-let numArr = [];
-let op = '';
-let num = '';
-let output = 0;
 
 const add = (a, b) => a + b;
 
@@ -18,8 +15,9 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 const operate = (op, arr) => {
+    numArr = numArr.map(num => Number(num));
+
     return numArr.reduce((total, num) => {
-        parseInt(num);
         return op == '+' ? add(total, num) 
               : op == '-' ? subtract(total, num) 
               : op == '*' ? multiply(total, num) 
@@ -27,15 +25,23 @@ const operate = (op, arr) => {
     });
 }
 
+const spliceArray = () => {
+    const lastLength = numArr[numArr.length - 1].length;
+    numArr.splice(numArr.length - lastLength, lastLength - 1);
+}
+
 const showOperator = (e) => {
     op = e.target.value;
-    numArr.push(num);
     num = '';
+    display.textContent = operate(op, numArr);
 }
 
 const showNumber = (e) => {
     num += e.target.textContent;
     display.textContent = num;
+
+    numArr.push(num);
+    spliceArray();
 }
 
 const showOutput = () => {
