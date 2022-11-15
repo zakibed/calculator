3d364994df549e9560611c688975f4d97aea9ca4
zakzinzan123 <zakzinzan123@gmail.com>
2022-11-15 17:02:36 +0800

    Change number storing and add check

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let numArr = [];
-let op, num = '';
 const display = document.querySelector('#output');
 const operators = document.querySelectorAll('#operators > button');
 const numbers = document.querySelectorAll('.number');
 const clear = document.querySelector('#clear');
 const equals = document.querySelector('#equals');
+let numArr = [];
+let check = true;
+let op, num = '';
 
 const add = (a, b) => a + b;
 
@@ -16,7 +17,6 @@ const divide = (a, b) => a / b;
 
 const operate = (op, arr) => {
     numArr = numArr.map(num => Number(num));
-
     return numArr.reduce((total, num) => {
         return op == '+' ? add(total, num) 
               : op == '-' ? subtract(total, num) 
@@ -25,42 +25,36 @@ const operate = (op, arr) => {
     });
 }
 
-const spliceArray = () => {
-    const lastLength = numArr[numArr.length - 1].length;
-    numArr.splice(numArr.length - lastLength, lastLength - 1);
-}
-
-const showOperator = (e) => {
+const getOperator = (e) => {
+    if (check == true) numArr.push(num);
     op = e.target.value;
     num = '';
+    check = false;
+
     display.textContent = operate(op, numArr);
 }
 
 const showNumber = (e) => {
     num += e.target.textContent;
     display.textContent = num;
-
-    numArr.push(num);
-    spliceArray();
+    check = true;
 }
 
-const showOutput = () => {
-    display.textContent = operate(op, numArr);
-}
-
-operators.forEach(btn => {
-    btn.addEventListener('click', showOperator);
-});
-
-numbers.forEach(btn => {
-    btn.addEventListener('click', showNumber);
-});
-
-equals.addEventListener('click', showOutput);
-
-clear.addEventListener('click', () => {
+const clearDisplay = () => {
     display.textContent = ''; 
     numArr = [];
     op = '';
     num = '';
-});
+}
+
+const showOutput = () => {
+    if (check == true) numArr.push(num);
+    check = false;
+
+    display.textContent = operate(op, numArr);
+}
+
+operators.forEach(btn => btn.addEventListener('click', getOperator));
+numbers.forEach(btn => btn.addEventListener('click', showNumber));
+clear.addEventListener('click', clearDisplay);
+equals.addEventListener('click', showOutput);
