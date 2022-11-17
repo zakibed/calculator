7f99ddc354ad90cf08baa378a12273006e0d0c32
zakzinzan123 <zakzinzan123@gmail.com>
2022-11-17 23:40:57 +0800

    Make calculator add decimals

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const display = document.querySelector('#output');
 const operators = document.querySelectorAll('#operators > button');
 const numbers = document.querySelectorAll('.number');
+const decimal = document.querySelector('#decimal');
 const clear = document.querySelector('#clear');
 const equals = document.querySelector('#equals');
 const error = document.querySelector('#error');
@@ -8,8 +9,8 @@ const error = document.querySelector('#error');
 let check = true;
 let numArr = [],
     opArr = [];
-let op = '', 
-    num = '';
+let op = '';
+let num = '';
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
@@ -59,6 +60,15 @@ const getNumber = (e) => {
     console.log(`number : ${display.textContent}`);
 }
 
+const addDecimal = () => {
+    if (!num[0]) num += '0';
+
+    if (!num.includes('.')) {
+        num += '.';
+        display.textContent = num;
+    }
+}
+
 const output = () => {
     if (!numArr.length) return;
 
@@ -78,11 +88,12 @@ const reset = () => {
     opArr = [];
     op = '';
     num = '';
-    display.textContent = ''; 
+    display.textContent = '0'; 
     error.style.display = 'none';
 }
 
 operators.forEach(btn => btn.addEventListener('click', getOperator));
 numbers.forEach(btn => btn.addEventListener('click', getNumber));
+decimal.addEventListener('click', addDecimal);
 equals.addEventListener('click', output);
 clear.addEventListener('click', reset);
