fb705f3501ae3d0a29a7ccd49f1bc448c6c20896
zakzinzan123 <zakzinzan123@gmail.com>
2023-03-21 19:25:58 +0800

    Add decimal btn functionality

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const topDisplay = document.querySelector('#display > div:first-of-type');
 const mainDisplay = document.querySelector('#display > div:last-of-type');
 const operatorBtns = document.querySelectorAll('.operator');
 const numberBtns = document.querySelectorAll('.number');
-const clearBtn = document.querySelector('#clear');
+const decimalBtn = document.querySelector('#decimal');
 const deleteBtn = document.querySelector('#delete');
+const clearBtn = document.querySelector('#clear');
 const percentBtn = document.querySelector('#percent');
 const equalsBtn = document.querySelector('#equals');
 let operatorClicked = false;
@@ -27,21 +28,28 @@ function clearAll() {
 }
 
 function calculate(num1, num2, op) {
+    currentNum = +currentNum;
+    previousNum = +previousNum;
+
     if (op === '+') solution = num1 + num2;
     if (op === '-') solution = num1 - num2;
     if (op === '×') solution = num1 * num2;
     if (op === '÷') solution = num1 / num2;
 
-    solution = Number.isFinite(solution) ? Number(solution.toFixed(5)) : '😵';
+    solution = Number.isFinite(solution) ? +solution.toFixed(5) : '😵';
 }
 
 function getNumber() {
-    if (operatorClicked || equalsClicked || !currentNum) {
+    if (
+        operatorClicked ||
+        equalsClicked ||
+        (!currentNum && !mainDisplay.textContent.includes('.'))
+    ) {
         mainDisplay.textContent = '';
     }
 
     mainDisplay.append(this.textContent);
-    currentNum = Number(mainDisplay.textContent);
+    currentNum = +mainDisplay.textContent;
 }
 
 function getOperator() {
@@ -60,14 +68,14 @@ function getOperator() {
 
     operatorClicked = true;
     previousOp = this.textContent;
-    previousNum = Number(mainDisplay.textContent);
+    previousNum = +mainDisplay.textContent;
 }
 
-function removeNumber() {
-    const display = mainDisplay.textContent;
+function getDecimal() {
+    if (mainDisplay.textContent.includes('.')) return;
 
-    mainDisplay.textContent = display.length > 1 ? display.slice(0, -1) : 0;
-    currentNum = Number(mainDisplay.textContent);
+    mainDisplay.append(this.textContent);
+    currentNum = mainDisplay.textContent;
 }
 
 function getAnswer() {
@@ -85,27 +93,29 @@ function getAnswer() {
     }
 
     equalsClicked = true;
-    previousNum = '';
     previousOp = '';
-    currentNum = Number(mainDisplay.textContent);
+    previousNum = 0;
+    currentNum = +mainDisplay.textContent;
+}
+
+function removeNumber() {
+    const display = mainDisplay.textContent;
+
+    mainDisplay.textContent = display.length > 1 ? display.slice(0, -1) : 0;
+    currentNum = +mainDisplay.textContent;
 }
 
 numberBtns.forEach((e) => e.addEventListener('click', getNumber));
-
 operatorBtns.forEach((e) => e.addEventListener('click', getOperator));
-
+decimalBtn.addEventListener('click', getDecimal);
 equalsBtn.addEventListener('click', getAnswer);
-
 deleteBtn.addEventListener('click', removeNumber);
-
 clearBtn.addEventListener('click', clearAll);
-
 document.querySelectorAll('button:not(#equals)').forEach((e) =>
     e.addEventListener('click', () => {
         equalsClicked = false;
     })
 );
-
 document.querySelectorAll('button:not(.operator)').forEach((e) =>
     e.addEventListener('click', () => {
         operatorClicked = false;
